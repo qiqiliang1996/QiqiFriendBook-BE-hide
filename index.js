bd900cfe535a83c514e56386317df963914a63b0
Qiqi Liang <qiqiliang1996@gmail.com>
2025-06-26 21:34:09 -0400

    fixed origin cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: 'https://friendbookfrontend.vercel.app/' }));
+app.use(cors({ origin: 'https://friendbookfrontend.vercel.app' }));
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
